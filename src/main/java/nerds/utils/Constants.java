ba40f481d172e0d9b07be421ad102cc01a02a376
Daniel Raygoza <raygoza.d@hotmail.com>
2022-01-11 18:57:49 -0700

    main

--- src/main/java/nerds/utils/Constants.java
+++ src/main/java/nerds/utils/Constants.java
@@ -1,10 +1,25 @@
 package nerds.utils;
-
+import edu.wpi.first.wpilibj.XboxController;
 public class Constants {
-    
+    //#region Controller-Buttons
+        /*Remap buttons by swapping the varible name itself around*/
+        //Buttons
+            public static final XboxController.Button A = XboxController.Button.kA;
+            public static final XboxController.Button B = XboxController.Button.kB;
+            public static final XboxController.Button X = XboxController.Button.kX;
+            public static final XboxController.Button Y = XboxController.Button.kY;
+            public static final XboxController.Button lB = XboxController.Button.kLeftBumper;
+            public static final XboxController.Button rB = XboxController.Button.kRightBumper;
+            public static final XboxController.Button lS = XboxController.Button.kLeftStick;
+            public static final XboxController.Button rS = XboxController.Button.kRightStick;
+        //Axis
+            public static final XboxController.Axis lS_Y = XboxController.Axis.kLeftY;
+            public static final XboxController.Axis lS_X = XboxController.Axis.kLeftX;
+            public static final XboxController.Axis rS_Y = XboxController.Axis.kRightY;
+            public static final XboxController.Axis rS_X = XboxController.Axis.kRightX;
+            public static final XboxController.Axis lT = XboxController.Axis.kLeftTrigger;
+            public static final XboxController.Axis rT = XboxController.Axis.kRightTrigger;
+    //#endregion
     // Measurements in cm
     public static final int upperHubHeight = 264;
-    
-    
-    
 }
